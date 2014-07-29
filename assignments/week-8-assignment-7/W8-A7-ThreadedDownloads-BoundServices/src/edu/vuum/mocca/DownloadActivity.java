60e49859585dea2a2acdb22715b6f4d9cd94cc57
archee <evangeem1@gmail.com>
2014-07-29 09:35:27 -0500

    fix merge conflicts from dropbox sync.

--- assignments/week-8-assignment-7/W8-A7-ThreadedDownloads-BoundServices/src/edu/vuum/mocca/DownloadActivity.java
+++ assignments/week-8-assignment-7/W8-A7-ThreadedDownloads-BoundServices/src/edu/vuum/mocca/DownloadActivity.java
@@ -71,12 +71,12 @@ public class DownloadActivity extends DownloadBase {
             @Override
             public void onServiceConnected(ComponentName name, IBinder service) {
             	Log.d(TAG, "ComponentName: " + name);
-                // TODO You fill in here to replace null with a call
+                // DONE You fill in here to replace null with a call
                 // to a generated stub method that converts the
                 // service parameter into an interface that can be
                 // used to make RPC calls to the Service.
 
-                mDownloadCall = null;
+                mDownloadCall = DownloadCall.Stub.asInterface(service);
             }
 
             /**
@@ -167,14 +167,25 @@ public class DownloadActivity extends DownloadBase {
 
     	switch (view.getId()) {
         case R.id.bound_sync_button:
-            // TODO - You fill in here to use mDownloadCall to
+            // DONE - You fill in here to use mDownloadCall to
             // download the image & then display it.
+            try {
+                displayBitmap(mDownloadCall.downloadImage(uri));
+            } catch (RemoteException e) {
+                Log.e(TAG, "RemoteException occurred. " + e.getMessage());
+            }
             break;
 
         case R.id.bound_async_button:
-            // TODO - You fill in here to call downloadImage() on
+            // DONE - You fill in here to call downloadImage() on
             // mDownloadRequest, passing in the appropriate Uri and
             // callback.
+            try {
+                mDownloadRequest.downloadImage(uri, mDownloadCallback);
+            } catch (RemoteException e) {
+                Log.e(TAG, "RemoteException occurred. " + e.getMessage());
+            }
+
             break;
         }
     }
